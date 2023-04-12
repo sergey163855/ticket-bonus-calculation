6197fea0c944e8b4b91cd08704862e4ab351fb7d
Demidoff Serg <serega.dem@inbox.ru>
2023-04-12 14:56:20 +0800

    add editing

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket_Price = 2000;
+        int ticketPrice = 2000;
         int bonus = 20;
 
         System.out.println("Итого бонусных милей вышло");
-        System.out.println(ticket_Price / bonus);
+        System.out.println(ticketPrice / bonus);
     }
 }
